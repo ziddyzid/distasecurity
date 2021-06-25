a222b4678e4f7631d9af8de3de0dd41011919387
ziddyzid <85139790+ziddyzid@users.noreply.github.com>
2021-06-25 09:52:25 -0400

    Add files via upload

---
+++ j.js
@@ -0,0 +1,10 @@
+function ope() {
+document.getElementById("bat").innerHTML = "Our officers are trained to observe your employees and your property and look for any suspicious activity. They are also trained to swiftly address any such activity and resolve any problems quickly and diligently.";
+}
+function ope1() {
+    document.getElementById("bat").innerHTML = "Our canine officers and their four-legged partners are your best bet for added security. From patrolling the perimeter of your property to sniffer services to keeping miscreants away, our canine services can accomplish a whole host of things.";
+}
+function ope2() {
+    document.getElementById("bat").innerHTML =  "Armed security guard services include uniformed standing guards, bodyguard and plainclothes security that accommodate your safety needs. Trained and certified guards for your specific requirements.";
+
+}
